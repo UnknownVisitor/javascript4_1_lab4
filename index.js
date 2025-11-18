d8072e8cb3d94dcd6a78854c616677fb54e0416a
UnknownVisitor <79756736+UnknownVisitor@users.noreply.github.com>
2025-11-18 10:31:10 +0000

    Com4_1.2

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express();
 
 const PI = Math.PI;
 
+// Obliczanie Pola i Obwodu Koła
 app.get('/math/circle/:r', (req, res) => {
-  
+
   const r = parseFloat(req.params.r);
 
   if (isNaN(r) || r <= 0) {
@@ -14,7 +15,6 @@ app.get('/math/circle/:r', (req, res) => {
   }
 
   const area = PI * r * r;
-
   const circumference = 2 * PI * r;
 
   const result = {
@@ -25,7 +25,34 @@ app.get('/math/circle/:r', (req, res) => {
   res.json(result);
 });
 
-const PORT = process.env.PORT || 5002;
+// Obliczanie Pola i Obwodu Prostokąta
+app.get('/math/rectangle/:width/:height', (req, res) => {
+
+  const width = parseFloat(req.params.width);
+  const height = parseFloat(req.params.height);
+
+  // Walidacja: Sprawdzenie, czy oba parametry są poprawnymi, dodatnimi liczbami
+  if (isNaN(width) || width <= 0 || isNaN(height) || height <= 0) {
+    return res.status(400).json({ error: "Szerokość i wysokość muszą być poprawnymi, dodatnimi liczbami." });
+  }
+
+  const area = width * height;
+
+  const perimeter = 2 * (width + height);
+
+  // Formatowanie i zwracanie odpowiedzi
+  const result = {
+    "area": area, 
+    "perimeter": perimeter
+  };
+
+  res.json(result);
+
+});
+
+
+
+const PORT = process.env.PORT || 5069;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
