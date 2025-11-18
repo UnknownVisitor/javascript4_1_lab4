05336f7065f7dd836a71bc8a4408fe1283aa8eb7
UnknownVisitor <79756736+UnknownVisitor@users.noreply.github.com>
2025-11-18 10:14:16 +0000

    Com4_1.1

--- index.js
+++ index.js
@@ -3,19 +3,29 @@
 const express = require('express');
 const app = express();
 
-// define endpoint for exercise 1 here
+const PI = Math.PI;
+
 app.get('/math/circle/:r', (req, res) => {
-//TODO1  
+  
+  const r = parseFloat(req.params.r);
+
+  if (isNaN(r) || r <= 0) {
+    return res.status(400).json({ error: "Promień musi być poprawną, dodatnią liczbą." });
+  }
+
+  const area = PI * r * r;
+
+  const circumference = 2 * PI * r;
+
+  const result = {
+    "area": area.toFixed(2),
+    "circumference": circumference.toFixed(2)
+  };
+
   res.json(result);
 });
 
-//TODO2
-
-
-//TODO3
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
